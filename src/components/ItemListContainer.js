f593850d008f380616b1d60b158051dc34d89b0a
Lautaro Settembrini <lautarosettembrini@hotmail.com>
2022-08-24 10:59:05 -0300

    3

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,16 @@
+const ItemListContainer = ({catalog}) => {
+    return (
+        <div>
+            <section>
+            <div>
+            <p className="catalog">
+            PRÓXIMAMENTE{catalog}
+            </p>
+            </div>
+            </section>
+        </div>
+        
+    )
+}
+
+export default ItemListContainer;
